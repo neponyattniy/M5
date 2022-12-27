b49fc3dc06028df468f020f5632b586cac274b17
Zaur <zauragayev1007@gmail.com>
2022-12-27 21:21:31 +0400

    almost done

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,6 +4,8 @@ import './MayLike.css'
 import './About.css'
 
 
+
+
 export default class About extends Component {
 
   render () {
@@ -16,19 +18,60 @@ export default class About extends Component {
 
         </header>
         <div className='cards'>
-          <Link to='comics1'></Link>
           <div className='card1'>
-            <p className='desc1'>Avengers Forever (2021) #12</p>
+            <p className='desc1'>
+              <a className='linkcom' href='https://www.marvel.com/comics/issue/104879/avengers_forever_2021_12'>
+              Avengers Forever (2021) #12
+              </a>
+          </p>
           </div>
           <div className='card2'>
-            <p className='desc1'></p>
+            <p className='desc1'>
+              <a className='linkcom' href='https://www.marvel.com/comics/issue/104561/dark_web_ms_marvel_2022_1'>
+              Dark Web: Ms. Marvel (2022) #1
+
+              </a>
+            </p>
           </div>
           <div className='card3'>
-            <p className='desc1'></p>
+            <p className='desc1'>
+              <a className='linkcom' href='https://www.marvel.com/comics/issue/100416/wakanda_2022_3'>
+              Wakanda (2022) #3
+
+              </a>
+            </p>
           </div>
-          <div className='card4'>
-            <p className='desc1'></p>
+          <div className='card1mobile'>
+            <p className='desc1mobile'>
+              <a className='linkcom' href='https://www.marvel.com/comics/issue/104879/avengers_forever_2021_12'>
+              Avengers Forever (2021) #12
+              </a>
+          </p>
           </div>
+          <div className='card2mobile'>
+            <p className='desc1mobile'>
+              <a className='linkcom' href='https://www.marvel.com/comics/issue/104561/dark_web_ms_marvel_2022_1'>
+              Dark Web: Ms. Marvel (2022) #1
+
+              </a>
+            </p>
+          </div>
+          <div className='card3mobile'>
+            <p className='desc1mobile'>
+              <a className='linkcom' href='https://www.marvel.com/comics/issue/100416/wakanda_2022_3'>
+              Wakanda (2022) #3
+
+              </a>
+            </p>
+          </div>
+          {/* <div className='card4'>
+            <p className='desc1'>
+              <a className='linkcom' href='https://www.marvel.com/comics/issue/96893/wolverine_2020_28'>
+              Wolverine (2020) #28
+
+              </a>
+            </p>
+          </div> */}
           
 
         </div>
